49fc7d11bc383a41490141854e2cb9bac92e604c
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-08-08 01:31:12 +0000

    remove debug code
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/widget-contexts/trunk@2405 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function() {
-	logScript('widget-contexts.js');
 	jQuery('.widget_context_toggle').livequery('change', function() {
 		var is_checked = jQuery(this).attr('checked');
 		jQuery(this).closest('div').find(':checkbox').attr('checked', is_checked);
 	});
-	logScript('end widget-contexts.js');
 });
