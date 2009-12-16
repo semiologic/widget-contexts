16783fe53772e5ce0172114b4687b95794381871
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-12-16 23:03:29 +0000

    bump
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/widget-contexts/trunk@3751 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- widget-contexts.php
+++ widget-contexts.php
@@ -3,7 +3,7 @@
 Plugin Name: Widget Contexts
 Plugin URI: http://www.semiologic.com/software/widget-contexts/
 Description: Lets you manage whether widgets should display or not based on the context.
-Version: 2.0.2 alpha
+Version: 2.0.2 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: widget-contexts
